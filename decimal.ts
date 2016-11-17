955340837424f2c970eefb516c9e93674f6bb665
Matthew Beatty <beattyml1@gmail.com>
2016-11-17 17:37:25 -0500

    Update decimal.ts

--- decimal.ts
+++ decimal.ts
@@ -1,51 +1,51 @@
 const checkAndFormat = (input: Decimal | number, operation: string) => {
-		if (this.precision !== input.precision) throw `Precision must be equal to ${operation} two numbers`;
-		return (typeof(input) === 'number') ? new Decimal(this.precision, input) : input;
+		if (this.p !== input.p) throw `p must be equal to ${operation} two numbers`;
+		return (typeof(input) === 'number') ? new Decimal(this.p, input) : input;
 }
 const compare = 'compare';
 
 export default class Decimal {
-	precision: number;
-	value: number;
-	constructor(precision: number, value: number) {
-		this.value = Math.round(this.value * Math.pow(10, this.precision));
-		this.precision = precision;
+	p: number;
+	v: number;
+	constructor(prec: number, val: number) {
+		this.v = Math.round(val * Math.pow(10, prec));
+		this.p = prec;
 	}
 
 	plus = (right: Decimal | number) => {
-		return new Decimal(this.precision, this.value + checkAndFormat(right, 'add').value);
+		return new Decimal(this.p, this.v + checkAndFormat(right, 'add').v);
 	}
 
 	minus =  (right: Decimal | number) => {
-		return new Decimal(this.precision, this.value - checkAndFormat(right, 'subtract').value);
+		return new Decimal(this.p, this.v - checkAndFormat(right, 'subtract').v);
 	}
 
 	times =  (right: Decimal | number) => {
-		return new Decimal(this.precision, this.value * checkAndFormat(right, 'multiply').value);
+		return new Decimal(this.p, this.v * checkAndFormat(right, 'multiply').v);
 	}
 
 	dividedBy = (right: Decimal | number) => {
-		return new Decimal(this.precision, this.value / checkAndFormat(right, 'divide').value);
+		return new Decimal(this.p, this.v / checkAndFormat(right, 'divide').v);
 	}
 
 	equals = (right: Decimal | number) => {
-		return this.value === checkAndFormat(right, compare).value;
+		return this.v === checkAndFormat(right, compare).v;
 	}
 
 	greaterThan = (right: Decimal | number) => {
-		return this.value > checkAndFormat(right, compare).value;
+		return this.v > checkAndFormat(right, compare).v;
 	}
 
 	lessThan = (right: Decimal | number) => {
-		return this.value < checkAndFormat(right, compare).value;
+		return this.v < checkAndFormat(right, compare).v;
 	}
 
 	greaterThanOrEqual = (right: Decimal | number) => {
-		return this.value >= checkAndFormat(right, compare).value;
+		return this.v >= checkAndFormat(right, compare).v;
 	}
 
 	lessThanOrEqual = (right: Decimal | number) => {
-		return this.value <= checkAndFormat(right, compare).value;
+		return this.v <= checkAndFormat(right, compare).v;
 	}
 
 	notEqual = (right: Decimal | number) => {
@@ -53,10 +53,10 @@ export default class Decimal {
 	}
 
 	toFixed = ()  =>{
-		return toNumber().toFixed(this.precision);
+		return toNumber().toFixed(this.p);
 	}
 
 	toNumber = ()   => {
-		return (this.value / Math.pow(10, this.precision)));
+		return (this.v / Math.pow(10, this.p)));
 	}
 }
